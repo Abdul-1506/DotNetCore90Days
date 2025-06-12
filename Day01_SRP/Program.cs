82d02d039b7547b1550b32932635477fd3c1843b
abdulrahimanIB <128671636+abdulrahimanIB@users.noreply.github.com>
2025-06-13 03:08:08 +0530

    Day 1 - SRP Principle

---
+++ Day01_SRP/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using static System.Collections.Specialized.BitVector32;
+
+namespace Day01_SRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userService = new UserService();
+            userService.CreateUser();
+
+           //Similarly for other services 
+
+            Console.WriteLine("Done!");
+        }
+    }
+
+
+
+    // Bad Approach as it voilates SRP - Single Responsible Principle
+    public class UserManager
+    {
+        public void CreateUser()
+        {
+
+        }
+
+        public void SendMail()
+        {
+
+        }
+
+        public void LogAction()
+        {
+
+        }
+    }
+
+
+
+    // This is Better approach (As it Follows SRP)
+    public class UserService
+    {
+        public void CreateUser()
+        {
+        }
+    }
+
+    public class MailService
+    {
+        public void SendMail()
+        {
+        }
+    }
+
+    public class LogService
+    {
+        public void LogAction()
+        {
+        }
+    }
+}
