c6ec3ae4972bde001a9f63723f56e119d5289e26
abdulrahimanIB <128671636+abdulrahimanIB@users.noreply.github.com>
2025-06-19 21:24:24 +0530

    Day 2

---
+++ Day02_OCP/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Day02_OCP
+{
+     class Program
+    {
+        static void Main(string[] args) { 
+            NotificationService service = new NotificationService();
+            service.notificationSend(new SMSNotificationSender());
+        }
+        
+
+    }
+
+    public class NotificationService
+    {
+        public void notificationSend(INotification notification)
+        {
+            notification.send();
+        }
+    }
+
+    public interface INotification
+    {
+        public void send();
+    }
+
+    public class EmailNotificationSender : INotification
+    {
+        public void send()
+        {
+            Console.WriteLine("Notification sending through Email");
+        }
+    }
+
+    public class SMSNotificationSender : INotification
+    {
+        public void send()
+        {
+            Console.WriteLine("Notification sending through SMS");
+        }
+    }
+}
